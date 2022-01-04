b200230a5e99938e88627ff1a08c8fec514c6136
DorianKr <dorian.krizaj@gmail.com>
2022-01-04 17:31:36 +0100

    Dodal mape za prevode

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import ReactDom from "react-dom";
 import App from './App';
 import './index.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+    <App />,
+    
+    document.getElementById('root')
+);
