317c6ec7326813dc6bf986649662793e6e42f4fd
DorianKr <dorian.krizaj@gmail.com>
2022-01-12 23:21:06 +0100

    Update App.js - Routes
    Dodal route in dolocil poti med stranmi.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Article, Feature, Navbar } from './components';
-import { Header, Footer, Features, Apartmaji } from './containers';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
-import Copyright from './components/copyright/Copyright';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Suspense } from 'react/cjs/react.production.min';
 
+import { Navbar, Copyright } from './components';
+import { Footer } from './containers';
+import { Home, Info } from './Pages';
+
+import './App.css';
+
+
+
 const App = () => {
+    const [toggleFooterCss, setToggleFooterCss] = useState(false);
     return (
         <Suspense fallback="Loading...">
-            <div className='App'>
-                <div className='nav__color'>
-                    <Router>
-                        <Navbar />
-                    </Router>
-                    <Header />
+            <Router>
+                <div className='App'>
+                    <Navbar />
+                    <Routes>
+                        <Route path='/Apartmaji-Jasna-Nika' element={<Home />} />
+                        <Route path='/Apartmaji-Jasna-Nika/info' element={<Info />} />
+                    </Routes>
+                    <div className='fill'></div>
+                    <Footer />
+                    <Copyright />
+
                 </div>
-                <Feature />
-                <Apartmaji />
-                <Footer />
-                <Copyright />
-            </div>
+            </Router>
         </Suspense>
     )
 }
