664c28d7c45200855c58cf28b234165424cc8a9d
DorianKr <dorian.krizaj@gmail.com>
2022-01-13 00:27:22 +0100

    Update Footer.jsx
    Refactoral translate

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -6,19 +6,19 @@ import './footer.css';
 const Footer = () => {
     const { t } = useTranslation();
     return (
-        <div className='__footer section__padding'>
+        <div className='__footer'>
             <div className='__footer_kontakti-section'>
-                <h4>{t('footer.kontaktirajte_nas')}</h4>
+                <h4>{t('info.kontaktirajte_nas')}</h4>
                 <p><RiPhoneLine className='react-icons' /> Tel: <a href="tel:+386000000000">000 000 000</a></p>
                 <p><RiMailOpenLine className='react-icons' /> Mail: <a href="mailto:">Apartmaji.Jasna-in-Nika@gmail.com</a></p>
             </div>
             <div className='__footer_input-section'>
-                <h4>{t('footer.vprasanja')}</h4>
+                <h4>{t('info.kontaktirajte_nas-vpr')}</h4>
                 <label htmlFor="mail">Mail:</label>
                 <input id='mail' type="mail" placeholder='primere@mail.com' />
-                <label htmlFor="text">{t('footer.msg')}</label>
+                <label htmlFor="text">{t('info.msg')}</label>
                 <textarea id='text' type="text" maxLength={500} />
-                <button type="submit">{t('button.poslji')}</button>
+                <button type="submit">{t('misc.poslji')}</button>
             </div>
             {/*
             <div>
@@ -31,7 +31,7 @@ const Footer = () => {
             </div>*/}
             <div className='__footer_license-section'>
                 <h4>{t('footer.licenca_ikonice')}</h4>
-                <p>{t('footer.ikonice_msg')} <a href="https://www.freepik.com" title="Freepik">Freepik</a> {t('footer.from')} <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+                <p>{t('footer.ikonice_msg')} <a href="https://www.freepik.com" title="Freepik">Freepik</a> {t('misc.from')} <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
                 <p></p>
             </div>
         </div>
