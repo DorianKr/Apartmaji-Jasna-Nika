f32dc6ef6888b477b1c5802e18a467e90bea15a4
DorianKr <dorian.krizaj@gmail.com>
2022-01-13 00:56:34 +0100

    Refactor Info.js
    Odstranil neuporabljen import za useState.

--- src/Pages/info/Info.jsx
+++ src/Pages/info/Info.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { RiPhoneFill, RiMailOpenFill, RiMapPin2Fill } from 'react-icons/ri';
 import { useTranslation } from 'react-i18next';
 
