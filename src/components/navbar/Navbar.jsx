193f764ebff46bfd41a36b3b154dd5e9f2605ac1
DorianKr <dorian.krizaj@gmail.com>
2022-01-13 00:21:42 +0100

    Update navbar links
    Spremenil Linke da dejansko spremenijo stran in dodal Link2 za linke, ki so na isti strani.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { RiMenu3Line, RiCloseLine, RiArrowDropDownLine, RiArrowDropUpLine } from 'react-icons/ri';
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link as Link2, animateScroll as scroll } from "react-scroll";
+import { Link } from 'react-router-dom';
 import i18n from '../../i18n';
 import { useTranslation } from 'react-i18next';
 import logo from '../../assets/logo-full.svg';
@@ -27,26 +28,26 @@ const Navbar = () => {
             <div className='__navbar-links'>
                 <div className='__navbar-links_container'>
                     <ul>
-                        <li ><Link to="home" >{t('nav.domov')}</Link></li>
+                        <li ><Link to="/Apartmaji-Jasna-Nika" >{t('nav.domov')}</Link></li>
                         <li className='dropdown'>
                             {toggleSubMenu
-                                ? <Link to="" onClick={() => setToggleSubMenu(false)}>{t('naslov_m')}<RiArrowDropDownLine color='#000' className='react-icons' size={21} /></Link>
-                                : <Link to="" onClick={() => setToggleSubMenu(true)}>{t('naslov_m')}<RiArrowDropUpLine color='#000' className='react-icons' size={21} /></Link>
+                                ? <Link to="#" onClick={() => setToggleSubMenu(false)}>{t('naslov_m')}<RiArrowDropDownLine color='#000' className='react-icons' size={21} /></Link>
+                                : <Link to="#" onClick={() => setToggleSubMenu(true)}>{t('naslov_m')}<RiArrowDropUpLine color='#000' className='react-icons' size={21} /></Link>
                             }
 
                             {!toggleSubMenu && (
                                 <ul className='submenu'>
-                                    <li><Link to='jasna 1' onClick={() => setToggleSubMenu(true)}>Jasna 1</Link></li>
-                                    <li><Link to='jasna 2' onClick={() => setToggleSubMenu(true)}>Jasna 2</Link></li>
-                                    <li><Link to='jasna 3' onClick={() => setToggleSubMenu(true)}>Jasna 3</Link></li>
-                                    <li><Link to='jasna 4' onClick={() => setToggleSubMenu(true)}>Jasna 4</Link></li>
-                                    <li><Link to='nika 1' onClick={() => setToggleSubMenu(true)}>Nika 1</Link></li>
-                                    <li><Link to='nika 2' onClick={() => setToggleSubMenu(true)}>Nika 2</Link></li>
+                                    <li><Link2 to='jasna 1' smooth={true} onClick={() => setToggleSubMenu(true)}>Jasna 1</Link2></li>
+                                    <li><Link2 to='jasna 2' smooth={true} onClick={() => setToggleSubMenu(true)}>Jasna 2</Link2></li>
+                                    <li><Link2 to='jasna 3' smooth={true} onClick={() => setToggleSubMenu(true)}>Jasna 3</Link2></li>
+                                    <li><Link2 to='jasna 4' smooth={true} onClick={() => setToggleSubMenu(true)}>Jasna 4</Link2></li>
+                                    <li><Link2 to='nika 1' smooth={true} onClick={() => setToggleSubMenu(true)}>Nika 1</Link2></li>
+                                    <li><Link2 to='nika 2' smooth={true} onClick={() => setToggleSubMenu(true)}>Nika 2</Link2></li>
                                 </ul>
                             )}
                         </li>
                         <li><Link to="zasedenost">{t('nav.zasedenost')}</Link></li>
-                        <li><Link to="info">{t('nav.info')}</Link></li>
+                        <li><Link to="/Apartmaji-Jasna-Nika/info">{t('nav.info')}</Link></li>
                         <li>
                             <select name="language" onChange={onChange}>
                                 <option value="sl">SLO</option>
@@ -69,29 +70,28 @@ const Navbar = () => {
                     <div className='__navbar-menu_container scale-up-center'>
 
                         <ul>
-                            <li className={toggleSubMenu ? '' : 'hide'}><Link to="home" onClick={() => setToggleMenu(false)}>{t('nav.domov')}</Link></li>
+                            <li className={toggleSubMenu ? '' : 'hide'}><Link to="/Apartmaji-Jasna-Nika" onClick={() => setToggleMenu(false)}>{t('nav.domov')}</Link></li>
                             <li className='dropdown'>
                                 {toggleSubMenu
-                                    ? <Link to="" onClick={() => { setToggleSubMenu(false); }}>{t('naslov_m')}<RiArrowDropDownLine color='#000' className='react-icons' size={21} /></Link>
-                                    : <Link to="" onClick={() => { setToggleSubMenu(true); }}>{t('naslov_m')}<RiArrowDropUpLine color='#000' className='react-icons' size={21} /></Link>
+                                    ? <Link to="#" onClick={() => { setToggleSubMenu(false); }}>{t('naslov_m')}<RiArrowDropDownLine color='#000' className='react-icons' size={21} /></Link>
+                                    : <Link to="#" onClick={() => { setToggleSubMenu(true); }}>{t('naslov_m')}<RiArrowDropUpLine color='#000' className='react-icons' size={21} /></Link>
                                 }
 
                                 {!toggleSubMenu && (
                                     <ul className='submenu'>
-                                        <li><Link to='jasna 1' onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Jasna 1</Link></li>
-                                        <li><Link to='jasna 2' onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Jasna 2</Link></li>
-                                        <li><Link to='jasna 3' onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Jasna 3</Link></li>
-                                        <li><Link to='jasna 4' onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Jasna 4</Link></li>
-                                        <li><Link to='nika 1' onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Nika 2</Link></li>
-                                        <li><Link to='nika 2' onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Nika 3</Link></li>
+                                        <li><Link2 to='jasna 1' smooth={true} onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Jasna 1</Link2></li>
+                                        <li><Link2 to='jasna 2' smooth={true} onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Jasna 2</Link2></li>
+                                        <li><Link2 to='jasna 3' smooth={true} onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Jasna 3</Link2></li>
+                                        <li><Link2 to='jasna 4' smooth={true} onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Jasna 4</Link2></li>
+                                        <li><Link2 to='nika 1' smooth={true} onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Nika 2</Link2></li>
+                                        <li><Link2 to='nika 2' smooth={true} onClick={() => { setToggleSubMenu(true); setToggleMenu(false); }}>Nika 3</Link2></li>
                                     </ul>
                                 )}
                             </li>
                             <li className={toggleSubMenu ? '' : 'hide'}><Link to="zasedenost" onClick={() => setToggleMenu(false)}>{t('nav.zasedenost')}</Link></li>
-                            <li className={toggleSubMenu ? '' : 'hide'}><Link to="info" onClick={() => setToggleMenu(false)}>{t('nav.info')}</Link></li>
+                            <li className={toggleSubMenu ? '' : 'hide'}><Link to="/Apartmaji-Jasna-Nika/info" onClick={() => setToggleMenu(false)}>{t('nav.info')}</Link></li>
 
                         </ul>
-
                     </div>
                 )
                 }
