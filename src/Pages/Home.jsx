911b918eec75164b207839c31f8e8633efd93bca
DorianKr <dorian.krizaj@gmail.com>
2022-01-13 00:31:17 +0100

    Dodal Strani
    Naredil posebaj datoteke za razlicen strani.

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header, Apartmaji } from '../containers';
+import { Feature } from '../components'
+function Home() {
+    return (
+        <><Header /><Feature /><Apartmaji /></>
+    )
+}
+
+export default Home
