78395f8970b1e2bd49f91f3cf07a4650d2471f61
DorianKr <dorian.krizaj@gmail.com>
2022-01-13 02:13:39 +0100

    Update Info.jsx
    Dodal title atribut v <ifframe/>

--- src/Pages/info/Info.jsx
+++ src/Pages/info/Info.jsx
@@ -20,7 +20,7 @@ function Info() {
                     <p><RiMapPin2Fill className='react-icons' /> {t('info.naslov')}: Put Oljica 5, 23206, Sukošan, {t('info.hr')}</p>
                     <div className='google-maps'>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4056.393881457728!2d15.328919579834206!3d44.035783156358946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4761e40a7b2f446d%3A0x35775f17caf6e47d!2sApartmani%20NIKA!5e0!3m2!1sen!2ssi!4v1641866843067!5m2!1sen!2ssi"
-                            allowFullScreen="" loading="lazy">
+                            title={'Map ' + t('naslov_e') + ' Nika'} allowFullScreen="" loading="lazy">
                         </iframe>
                     </div>
                 </div>
@@ -29,7 +29,7 @@ function Info() {
                     <p><RiMapPin2Fill className='react-icons' /> {t('info.naslov')}: Sveti Petar 39, 23207, Sveti Petar na Moru, {t('info.hr')}</p>
                     <div className='google-maps'>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1434.8612528950036!2d15.367918157089706!3d44.00646209264764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4761e1117ab15159%3A0xac2d7f3935460130!2sJasna!5e0!3m2!1sen!2ssi!4v1641867149113!5m2!1sen!2ssi"
-                            allowFullScreen="" loading="lazy">
+                            title={'Map ' + t('naslov_e') + ' Jasna'} allowFullScreen="" loading="lazy">
                         </iframe>
                     </div>
                 </div>
