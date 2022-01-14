320de509babe0bd1d0e7cd87e3cfb4f63de37a5a
DorianKr <dorian.krizaj@gmail.com>
2022-01-14 01:39:33 +0100

    Dodal Apartma med strani
    V app router definiral poti do strani posameznega apartmaja

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Suspense } from 'react/cjs/react.production.min';
 
 import { Navbar, Copyright } from './components';
 import { Footer } from './containers';
-import { Home, Info } from './Pages';
+import { Apartma, Home, Info } from './Pages';
 
 import './App.css';
 
@@ -21,11 +21,16 @@ const App = () => {
                     <Routes>
                         <Route path='/Apartmaji-Jasna-Nika' element={<Home />} />
                         <Route path='/Apartmaji-Jasna-Nika/info' element={<Info />} />
+                        <Route path='/Apartmaji-Jasna-Nika/Jasna_1' element={<Apartma id={'j1'} />} />
+                        <Route path='/Apartmaji-Jasna-Nika/Jasna_2' element={<Apartma id={'j2'} />} />
+                        <Route path='/Apartmaji-Jasna-Nika/Jasna_3' element={<Apartma id={'j3'} />} />
+                        <Route path='/Apartmaji-Jasna-Nika/Jasna_4' element={<Apartma id={'j4'} />} />
+                        <Route path='/Apartmaji-Jasna-Nika/Nika_1' element={<Apartma id={'n1'} />} />
+                        <Route path='/Apartmaji-Jasna-Nika/Nika_2' element={<Apartma id={'n2'} />} />
                     </Routes>
                     <div className='fill'></div>
                     <Footer />
                     <Copyright />
-
                 </div>
             </Router>
         </Suspense>
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from './Home';
 export { default as Info } from './info/Info';
+export { default as Apartma } from './apartma/Apartma';
