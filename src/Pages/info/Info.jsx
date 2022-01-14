aaea0dcecb885f17716cb74eec25538d89d2168f
DorianKr <dorian.krizaj@gmail.com>
2022-01-14 01:38:05 +0100

    Update Info page
    Dodal del z pogostimi vprasanji.

--- src/Pages/info/Info.jsx
+++ src/Pages/info/Info.jsx
@@ -14,7 +14,6 @@ function Info() {
                 <p><RiMailOpenFill className='react-icons' /> Mail: <a href="mailto:">Apartmaji.Jasna-in-Nika@gmail.com</a></p>
             </div>
             <div className='flex_row __info_section'>
-
                 <div>
                     <h3>{t('naslov_e')} Nika</h3>
                     <p><RiMapPin2Fill className='react-icons' /> {t('info.naslov')}: Put Oljica 5, 23206, Sukošan, {t('info.hr')}</p>
@@ -33,6 +32,27 @@ function Info() {
                         </iframe>
                     </div>
                 </div>
+            </div>
+            <div className='_vprasanja'>
+                <h2>{t('info.vprasanja')}</h2>
+                <center>
+                    <details>
+                        <summary>Ali je plaža blizu?</summary>
+                        <p>JA! Plaža je blizu, samo kakšnih 60m (2 min peš).</p>
+                    </details>
+                    <details>
+                        <summary>Ali je kakšen prostor za druženje zunaj?</summary>
+                        <p>Ja. Vsaka hiša ima zunaj urejen kotiček z žarom in mizo.</p>
+                    </details>
+                    <details>
+                        <summary>Ali imajo vsi apartmaji klimo?</summary>
+                        <p>Ja. Vsak amaprtma ima svojo klimatsko napravo in daljinec za vpravljanje.</p>
+                    </details>
+                    <details>
+                        <summary>Ali je za klimo potrebno doplačilo?</summary>
+                        <p>Ne! Klima ni posebno zaračunana.</p>
+                    </details>
+                </center>
             </div>
         </div>
     )
