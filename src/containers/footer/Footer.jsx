359adabfb6ec82f8c6b3309dcbff06fb6dd7b373
DorianKr <dorian.krizaj@gmail.com>
2022-01-24 23:35:31 +0100

    Footer.jsx clean up

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -15,20 +15,11 @@ const Footer = () => {
             <div className='__footer_input-section'>
                 <h4>{t('info.kontaktirajte_nas-vpr')}</h4>
                 <label htmlFor="mail">Mail:</label>
-                <input id='mail' type="mail" placeholder='primere@mail.com' />
+                <input id='mail' type="mail" placeholder={t('misc.mail')} />
                 <label htmlFor="text">{t('info.msg')}</label>
                 <textarea id='text' type="text" maxLength={500} />
                 <button type="submit">{t('misc.poslji')}</button>
             </div>
-            {/*
-            <div>
-                 <div class="mapouter">
-                    <div class="gmap_canvas">
-                        <iframe width="400" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=Apartmani%20NIKA,%20Put%20Oljica,%20Suko%C5%A1an,%20Croatia&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                        <a href="https://fmovies-online.net">fmovies</a>
-                    </div>
-                </div> 
-            </div>*/}
             <div className='__footer_license-section'>
                 <h4>{t('footer.licenca_ikonice')}</h4>
                 <p>{t('footer.ikonice_msg')} <a href="https://www.freepik.com" title="Freepik">Freepik</a> {t('misc.from')} <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
@@ -37,5 +28,4 @@ const Footer = () => {
         </div>
     )
 }
-
 export default Footer
